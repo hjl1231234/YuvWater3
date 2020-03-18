dded2730f40d3709f4d7a38829033f1f231ffc30
hejl1111 <hejl1111@thundersoft.com>
2020-03-18 21:13:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yuvwater3/monitor/MonitorVideoManager.java
@@ -0,0 +1,49 @@
+package com.example.yuvwater3.monitor;
+
+
+import com.example.yuvwater3.utils.Utils;
+
+public class MonitorVideoManager {
+    private static MonitorVideoManager sInstance;
+
+    private String TAG = "MonitorVideoManager";
+
+    private MonitorVideoManager() {
+
+    }
+
+    public static MonitorVideoManager getInstance() {
+        if (sInstance == null) {
+            synchronized (MonitorVideoManager.class) {
+                if (sInstance == null)
+                    sInstance = new MonitorVideoManager();
+            }
+        }
+        return sInstance;
+    }
+
+    private int mWidth, mHeight;
+
+    public void setWidth(int width) {
+        this.mWidth = width;
+    }
+
+    public void setHeight(int height) {
+        this.mHeight = height;
+    }
+
+    public void onCameraData(byte[] nv21Data) {
+        MuxerManager.getInstance().addVideoFrameData(nv21Data);
+    }
+
+    public void start() {
+        String newVideoPath = Utils.getVideoFilePath();
+        MuxerManager.getInstance().reStartMuxer(newVideoPath);
+    }
+
+
+    public void stop() {
+        MuxerManager.getInstance().stopMuxer();
+    }
+
+}
